17137e81f099da60cb900093eddaecd985bb6748
Micheal_Zhang <2519443269@qq.com>
2024-05-09 18:27:39 +0800

    zh:WebDisk

---
+++ build/CMakeFiles/WebDisk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebDisk.dir/src/main.cpp.o"
+  "../bin/WebDisk.pdb"
+  "../bin/WebDisk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebDisk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
